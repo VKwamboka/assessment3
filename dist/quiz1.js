a9daf9f4541e478a37786cdcb30c81bd95ac108c
Verah Nyagaka <verahnyagaka101@gmail.com>
2023-01-28 18:14:26 +0300

    updated code

--- dist/quiz1.js
+++ dist/quiz1.js
@@ -1,28 +1,106 @@
 "use strict";
-class Balance {
-    constructor(balance, income, expense) {
-        this.balance = balance;
-        this.balance = balance;
-        this.income = income;
-        this.expense = expense;
-    }
-    totalBalance() {
-        return this.balance = this.income - this.expense;
-    }
-}
+const content1 = document.getElementById("content1");
+let item = document.getElementById("item");
+const btn = document.getElementById("create-btn");
+let expense = document.getElementById("expense");
+let amountValue = document.getElementById("amount");
+let incomeValue = document.getElementById("incomes");
+let expensediv = document.getElementById("expensediv");
+// class Balance implements Records {
+//   static income: number;
+//   static expense: number;
+//   //static totalIncome:number;
+//   constructor(private balance: number, income: number, expense: number) {
+//     this.balance = balance;
+//     //this.income = income;
+//     //this.expense = expense;
+//   }
+//   getTotalBalance(): number {
+//     return Balance.income - Balance.expense
+//   }
+// }
 class Expense {
+    // public totalExpenses:number = 0
+    // date: string;
     constructor(amount) {
         this.amount = amount;
+        // this.date = date;
     }
 }
 class Income {
-    constructor(amount) {
-        this.amount = amount;
+    // date: string;
+    // public totalIncome: number = 0;
+    constructor(amountIncome) {
+        this.amountIncome = amountIncome;
+        // this.date = date;
+        // this.addIncome(this.amount)
     }
 }
-const income = document.getElementById("income");
-const amount = document.getElementById("amount");
-const date = document.getElementById("date");
-let item = document.getElementById("citem");
-function showItems() {
+const Usage = [];
+btn.addEventListener("click", showExpense);
+function showExpense() {
+    item.innerHTML = "";
+    const amount = amountValue.value;
+    let singleExpense = amount;
+    console.log(singleExpense);
+    Usage.push(singleExpense);
+    console.log(Usage);
+    let html = `
+    <div class="income">
+      <h1>${Usage}</h1>
+                   
+    </div>`;
+    item.innerHTML += html;
 }
+const Salary = [];
+btn.addEventListener("click", showIncome);
+function showIncome() {
+    expensediv.innerHTML = '';
+    const amountIncome = incomeValue.value;
+    let singleIncome = amountIncome;
+    console.log(singleIncome);
+    Salary.push(singleIncome);
+    console.log(Salary);
+    console.log(incomeValue);
+    console.log(amountValue);
+    let html = `
+  <div class="income">
+    <h1>${Salary}</h1>
+                 
+  </div>`;
+    expensediv.innerHTML += html;
+}
+// function addIncomeRecord(){
+//   let amountInput = document.querySelector('#amounts') as HTMLInputElement;
+//   let date = document.querySelector('#date')?.nodeValue;
+//   let amount = amountInput.value
+//   const incomeRecord = new Income(amount, date)
+// }
+// function showIncome() {
+//   let income = new Income();
+//   content1.addEventListener("submit", (e) => {
+//     e.preventDefault();   
+//     let html = `
+//             <div class="income">
+//                     <h1>${income.amount}</h1>
+//                     <p>${income.date}</p>                
+//             </div>`;
+//     item.innerHTML += html;
+//   });
+// }
+// btn.addEventListener("click", showIncome);
+// showIncome();
+// function tracker(){
+//   // income
+//   const amountIncome = incomeValue.value
+//   let singleIncome:Income=amountIncome
+//   console.log(singleIncome)
+//   Salary.push(singleIncome)
+//   console.log(Salary)
+//   // EXPENSES
+//   const amount = amountValue.value
+//   let singleExpense:Expense=amount
+//   console.log(singleExpense)
+//   Usage.push(singleExpense)
+//   console.log(Usage)
+// }
--- src/quiz1.ts
+++ src/quiz1.ts
@@ -1,57 +1,155 @@
-interface Records{
-    income:number
-    expense: number
-    
-   
-}
-interface Incomes{
-    amount:number;
-}
-
-interface Expenses{
-    amount:number;
-}
-
-
-class Balance implements Records{
-    income:number;
+const content1 = document.getElementById("content1")! as HTMLFormElement;
+let item = document.getElementById("item")! as HTMLDivElement;
+const btn = document.getElementById("create-btn")! as HTMLButtonElement;
+let expense = document.getElementById("expense")! as HTMLDivElement;
+let amountValue = document.getElementById("amount")! as HTMLFormElement;
+let incomeValue = document.getElementById("incomes")! as HTMLFormElement;
+let expensediv = document.getElementById("expensediv")! as HTMLFormElement;
+interface Records {
+  income: number;
   expense: number;
-  
+  date: string;
+}
+interface Incomes {
+  amountIncome: number;
+  // date?: string;
+}
 
-    constructor( private balance: number, income: number, expense: number){
-        this.balance = balance
-        this.income = income
-        this.expense = expense
-    }
+interface Expenses {
+  amount: number;
+  // date: string;
+}
 
-    totalBalance():number {
-        return this.balance = this.income - this.expense
-         }
+// class Balance implements Records {
+//   static income: number;
+//   static expense: number;
+//   //static totalIncome:number;
+
+//   constructor(private balance: number, income: number, expense: number) {
+//     this.balance = balance;
+//     //this.income = income;
+//     //this.expense = expense;
+//   }
+
+//   getTotalBalance(): number {
+//     return Balance.income - Balance.expense
+//   }
+
+// }
+
+class Expense implements Expenses {
+  amount: number;
+  // public totalExpenses:number = 0
+  // date: string;
+  constructor(amount: number) {
+    this.amount = amount;
+    // this.date = date;
+  }
 
 
 }
 
-class Expense implements Expenses{
-amount: number;
-constructor(amount:number){
-    this.amount = amount
+class Income implements Incomes {
+  amountIncome: number;
+  // date: string;
+  // public totalIncome: number = 0;
+
+  constructor(amountIncome: number) {
+    this.amountIncome = amountIncome;
+    // this.date = date;
+    // this.addIncome(this.amount)
+  }
+
+  // addIncome(amount: number) {
+  //   this.totalIncome += amount;
+  //   // Balance.income += amount;
+  // }
 }
+
+
+const Usage:Expense[]=[]
+
+btn.addEventListener("click", showExpense)
+
+function showExpense(){
+  item.innerHTML =""
+  const amount = amountValue.value
+  let singleExpense:Expense=amount
+  console.log(singleExpense)
+  Usage.push(singleExpense)
+  console.log(Usage)
+
+  let html = `
+    <div class="income">
+      <h1>${Usage}</h1>
+                   
+    </div>`;
+
+    item.innerHTML += html;
 
 }
 
-class Income implements Incomes{
-    amount: number;
-constructor(amount:number){
-    this.amount = amount
-}
-}
+const Salary:Income[]=[]
 
+btn.addEventListener("click", showIncome)
+function showIncome(){
+  expensediv.innerHTML = ''
+  const amountIncome = incomeValue.value
+  let singleIncome=amountIncome
+  console.log(singleIncome)
+  Salary.push(singleIncome)
+  console.log(Salary)
+  console.log(incomeValue)
+  console.log(amountValue)
 
-const income = document.getElementById("income")! as HTMLInputElement
-const amount= document.getElementById("amount")! as HTMLInputElement
-const date = document.getElementById("date")! as HTMLInputElement
-let item = document.getElementById("item")! as HTMLDivElement
+  let html = `
+  <div class="income">
+    <h1>${Salary}</h1>
+                 
+  </div>`;
 
-function showItems(){
-    
+  expensediv.innerHTML += html;
 }
+// function addIncomeRecord(){
+//   let amountInput = document.querySelector('#amounts') as HTMLInputElement;
+//   let date = document.querySelector('#date')?.nodeValue;
+//   let amount = amountInput.value
+
+//   const incomeRecord = new Income(amount, date)
+
+// }
+
+// function showIncome() {
+//   let income = new Income();
+
+//   content1.addEventListener("submit", (e) => {
+//     e.preventDefault();   
+
+//     let html = `
+//             <div class="income">
+//                     <h1>${income.amount}</h1>
+//                     <p>${income.date}</p>                
+//             </div>`;
+
+//     item.innerHTML += html;
+//   });
+// }
+
+// btn.addEventListener("click", showIncome);
+// showIncome();
+// function tracker(){
+//   // income
+//   const amountIncome = incomeValue.value
+//   let singleIncome:Income=amountIncome
+//   console.log(singleIncome)
+//   Salary.push(singleIncome)
+//   console.log(Salary)
+
+//   // EXPENSES
+//   const amount = amountValue.value
+//   let singleExpense:Expense=amount
+//   console.log(singleExpense)
+//   Usage.push(singleExpense)
+//   console.log(Usage)
+
+// }
